216863b874c8c4384e084457fdacd7847903dbaf
DiegoBarreras <diegobarreras01@gmail.com>
2026-05-19 15:27:09 -0600

    Agregar dominio al CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors({ 
   origin: [
     'http://localhost:5173',
-    'http://192.168.100.59:5173'
+    'https://zerokauth-frontend.onrender.com/'
   ] 
 }));
 app.use(express.json());
