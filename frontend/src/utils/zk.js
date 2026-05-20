bbceae943ff5557f8052017a928fd43db0400a5d
DiegoBarreras <diegobarreras01@gmail.com>
2026-05-20 04:09:02 -0600

    Update para parsear el secreto a numero

--- frontend/src/utils/zk.js
+++ frontend/src/utils/zk.js
@@ -1,25 +1,33 @@
 import * as snarkjs from 'snarkjs';
 import { buildPoseidon } from 'circomlibjs';
 
+function base32ToNumber(base32) {
+  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+  let result = 0n
+  for (const char of base32.toUpperCase().replace(/=+$/, '')) {
+    const val = alphabet.indexOf(char)
+    if (val === -1) continue
+    result = result * 32n + BigInt(val)
+  }
+  return result
+}
+
 export async function generarPruebaZK(secreto, challenge) {
-  const poseidon = await buildPoseidon();
+  const poseidon = await buildPoseidon()
 
-  const secretoBig = BigInt(secreto);
-  const challengeBig = BigInt(challenge);
-
-  const hash = poseidon([secretoBig, challengeBig]);
-  const hashStr = poseidon.F.toString(hash);
+  const secretoBig = base32ToNumber(secreto)
+  const challengeBig = BigInt(challenge)
 
   const input = {
     secreto: secretoBig.toString(),
     challenge: challengeBig.toString(),
-  };
+  }
 
   const { proof, publicSignals } = await snarkjs.groth16.fullProve(
     input,
     '/zk/totp.wasm',
     '/zk/totp_final.zkey'
-  );
+  )
 
-  return { proof, publicSignals, hash: hashStr };
+  return { proof, publicSignals }
 }
